5700d7b45b54fe1274cef79ad8e5cccc018ba930
Thomas Justin Krueger <Tomjk@acdmail.com>
2016-10-31 14:09:56 -0400

    Inital Upload: Semi Working Code

---
+++ Lab06/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab06
+{
+    public class Program
+    {
+
+        public static string vowels = "aeiou";
+
+        static void Main(string[] args)
+        {
+            string input = getInput();
+
+            Console.WriteLine(input);
+
+            Console.WriteLine(TranslateWords(input));
+
+            Console.ReadKey();
+
+        }
+
+
+        //Input
+        static string getInput()
+        {
+            return Console.ReadLine();
+        }
+
+        //Processing
+        public static string TranslateWords(string words)
+        {
+            string[] arrayOfWords = words.Split(' ');
+            StringBuilder finalSentance = new StringBuilder();
+            string finalString;
+            foreach (string word in arrayOfWords)
+            {
+                finalSentance.Append(TranslateWord(word) + " ");
+            }
+
+            
+            finalString = ((finalSentance.ToString()).ToLower()).TrimEnd();
+            return CapitalizeFirstLetter(finalString);
+
+            //return finalString;
+        }
+
+        public static string CapitalizeFirstLetter(string word)
+        {
+            string newString = (word[0].ToString()).ToUpper();
+
+            newString += word.Substring(1);
+            return newString;
+        }
+        
+        public static string TranslateWord(string word)
+        {
+            string subStringToKeep;
+            string subStringToMove;
+            string translatedString = "";
+
+            if (positionOf1stVowel(word) == -1)
+            {
+                Console.WriteLine("There are no vowels!!! I break.");
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
+
+            subStringToKeep = word.Substring(positionOf1stVowel(word));
+            subStringToMove = word.Substring(0, positionOf1stVowel(word));
+
+
+            translatedString += subStringToKeep;
+            translatedString += subStringToMove;
+
+            if (!(vowels.Contains(word[0])))
+                translatedString += "ay";
+            else translatedString += "way";
+
+
+            return translatedString;
+        }
+
+        static int positionOf1stVowel(string word)
+        {
+            for (int letterIndex = 0; letterIndex < word.Length; letterIndex++)
+            {
+                if (vowels.Contains(word.ElementAt(letterIndex)))
+                {
+                    return letterIndex;
+                }
+            }
+            return -1;
+        }
+
+        //Output
+
+    }
+}
---
+++ PigLatinTranslator/UnitTest1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Lab06;
+
+namespace PigLatinTranslator
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void translateWord()
+        {
+            string RealWord = Program.TranslateWord("this");
+            string ExpectedWord = "isthay";
+            Assert.AreEqual(ExpectedWord, RealWord);
+
+        }
+
+        [TestMethod]
+        public void translateWords()
+        {
+            string RealWord = Program.TranslateWords("this sentance");
+            string ExpectedWord = "Isthay entencesay";
+            Assert.AreEqual(ExpectedWord, RealWord);
+
+        }
+        [TestMethod]
+        public void translateWordsToLower()
+        {
+            string RealWord = Program.TranslateWord("this");
+            string ExpectedWord = "isthay";
+            Assert.AreEqual(ExpectedWord, RealWord);
+
+        }
+    }
+}
