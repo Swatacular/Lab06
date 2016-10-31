e5871e04926b8f304310ca87137b147ac112a54b
Thomas Justin Krueger <Tomjk@acdmail.com>
2016-10-31 14:53:30 -0400

    Added comments and made it look nice :D

--- Lab06/Program.cs
+++ Lab06/Program.cs
@@ -9,7 +9,7 @@ namespace Lab06
     public class Program
     {
 
-        public static string vowels = "aeiou";
+        public static string vowels = "aeiouAEIOU";
 
         static void Main(string[] args)
         {
@@ -18,12 +18,15 @@ namespace Lab06
                 Console.Clear();
                 Console.WriteLine("Welcome to the Pig Latin Translator!");
                 Console.Write("Line to translate: ");
+                //super simple method to get the users input.
                 string input = getInput();
 
                 Console.WriteLine();
                 Console.WriteLine("Press any key to translate. . .");
                 Console.Clear();
                 Console.Write("Your translated line: ");
+
+                //method to translate the entire sentance and return with a capitalized first letter (all the rest lower)
                 Console.WriteLine(TranslateWords(input));
                 Console.WriteLine();
                 Console.Write("Continue? (y/n): ");
@@ -45,6 +48,8 @@ namespace Lab06
         public static string TranslateWords(string words)
         {
             string[] arrayOfWords = words.Split(' ');
+
+            //build apon this StringBuilder for the final string.
             StringBuilder finalSentance = new StringBuilder();
             string finalString;
             foreach (string word in arrayOfWords)
@@ -52,7 +57,8 @@ namespace Lab06
                 finalSentance.Append(TranslateWord(word) + " ");
             }
 
-            
+            //since after every word is a space, we need to trim it.
+            //we could lower the 
             finalString = ((finalSentance.ToString()).ToLower()).TrimEnd();
             return CapitalizeFirstLetter(finalString);
 
@@ -61,8 +67,10 @@ namespace Lab06
 
         public static string CapitalizeFirstLetter(string word)
         {
+            //gets the first letter and capitalizes it.
             string newString = (word[0].ToString()).ToUpper();
-
+            
+            //takes that first letter and adds the rest of the string starting at position 1 (second letter)
             newString += word.Substring(1);
             return newString;
         }
@@ -75,18 +83,27 @@ namespace Lab06
 
             if (positionOf1stVowel(word) == -1)
             {
+                //probably could work on this a little more.. but whatev...
+                Console.Clear();
                 Console.WriteLine("There are no vowels!!! I break.");
                 Console.ReadKey();
                 Environment.Exit(0);
             }
 
+            //gets the second half of the word
             subStringToKeep = word.Substring(positionOf1stVowel(word));
+
+
+            //gets the first "half" of the word
             subStringToMove = word.Substring(0, positionOf1stVowel(word));
 
 
+            //rearranges the halves.
             translatedString += subStringToKeep;
             translatedString += subStringToMove;
 
+
+            //adds the way/ay depending on the original words first letter
             if (!(vowels.Contains(word[0])))
                 translatedString += "ay";
             else translatedString += "way";
@@ -97,6 +114,7 @@ namespace Lab06
 
         static int positionOf1stVowel(string word)
         {
+            //checking each position for a vowel
             for (int letterIndex = 0; letterIndex < word.Length; letterIndex++)
             {
                 if (vowels.Contains(word.ElementAt(letterIndex)))
@@ -106,8 +124,5 @@ namespace Lab06
             }
             return -1;
         }
-
-        //Output
-
     }
 }
